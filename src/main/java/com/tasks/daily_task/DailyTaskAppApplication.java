7f1b8d06b825d18c0e0b145d6185fc02e9500332
Dilshan Subhashana Rajapaksha <59592282+rdsubhashana1994@users.noreply.github.com>
2020-10-20 06:42:46 +0530

    first commit
    first initial commit

---
+++ src/main/java/com/tasks/daily_task/DailyTaskAppApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.daily_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTaskAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTaskAppApplication.class, args);
+    }
+
+}
